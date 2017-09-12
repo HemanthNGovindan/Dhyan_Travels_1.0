feb08df5bb7106488e67b9e85081423195ff8ccf
Hemanth Govindan <hemanth.govindan@sapiens.com>
2017-09-12 18:37:38 +0530

    other components

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer, Vehicle, EnquiryRequest, ContactUsRequest } from '../../models/models';
+import { APIService } from '../../functions/api/services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidateField } from '../../functions/directives';
+import { CustomValidationRules, ValidationMessages, ApplicationConstants } from '../../functions/constants';
+
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
-
+  public contactUsRequestObject: ContactUsRequest;
+  public contactUsForm: FormGroup;
+  public clientInfoTableClass: string[];
+  private validationMessages = ValidationMessages;
+  private Submitted: boolean = false;
+  constructor(private fb: FormBuilder, private APIService: APIService) {
+      this.contactUsRequestObject = new ContactUsRequest(new Customer('', '', ''));
+      this.clientInfoTableClass = ["clientInfoTable"]
+  }
   ngOnInit() {
+      this.buildForm();
+  }
+
+  buildForm(): void {
+      this.Submitted = false;
+      this.contactUsForm = this.fb.group({
+          'FullName': [this.contactUsRequestObject.Customer.FullName, [Validators.required, ValidateField(new CustomValidationRules('FullName'), this.contactUsForm)]],
+          'PhoneNumber': [this.contactUsRequestObject.Customer.PhoneNumber, [Validators.required, ValidateField(new CustomValidationRules('PhoneNumber'), this.contactUsForm)]],
+          'EmailId': [this.contactUsRequestObject.Customer.EmailId, [Validators.required, ValidateField(new CustomValidationRules('EmailId'), this.contactUsForm)]]
+      });
+  }
+
+  public ContactUsText = "Contact Us";
+  NavigateToFacebook() {
+      window.open("https://www.facebook.com/cocacola", "_blank");
+  }
+
+  ReturnValid = function (controlName: string) {
+      var returnValue = '';
+      if (controlName !== "") {
+          if (this.contactUsForm.controls[controlName].errors !== null) {
+              if (this.contactUsForm.controls[controlName].errors.message !== undefined) {
+                  return this.contactUsForm.controls[controlName].errors.message;
+              }
+              else if (this.contactUsForm.controls[controlName].errors.required === true && this.Submitted) {
+                  return this.validationMessages.Messages[controlName + '_Required'];
+              }
+              else {
+                  '';
+              }
+          }
+      }
+  }
+
+  SubmitContactRequest = function (action: string) {
+      if (action === ApplicationConstants.CustomerAction.SUBMIT) {
+          this.Submitted = true;
+          if (this.contactUsForm.valid) {
+              this.contactUsRequestObject.Customer = new Customer(this.contactUsForm.FullName, this.contactUsForm.PhoneNumber, this.contactUsForm.EmailId);
+              console.log(this.contactUsRequestObject);
+          }
+      }
+      else if (action === ApplicationConstants.CustomerAction.CLEAR) {
+          this.buildForm();
+      }
+      return;
   }
 
 }
--- src/app/components/vehicles/vehicles.component.ts
+++ src/app/components/vehicles/vehicles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from '../../functions/api/services';
+import { Vehicle } from '../../models/models';
 
 @Component({
   selector: 'app-vehicles',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiclesComponent implements OnInit {
 
-  constructor() { }
+  public cabObject: Vehicle[];
+  constructor(private APIService: APIService) {
+    this.cabObject = [new Vehicle('id', '', 7, 13, 'Toyota - AC', 'Preferable for large families'),
+    new Vehicle('id', '', 5, 10, 'Indica - AC', 'Preferable for small families'),
+    new Vehicle('id', '', 5, 10, 'Indica - Non - AC', 'Preferable for small families'),
+    new Vehicle('id', '', 5, 10, 'Toyota - Non - AC', 'Preferable for small families'),
+    new Vehicle('id', '', 8, 10, 'Shya - Non - AC', 'Preferable for heavy families')];
+
+  }
 
   ngOnInit() {
   }
